f0ece51a3e4f4ae440e2c3f9e45bb60a47ca8151
Sean Lovinger <lovingersean01@gmail.com>
2015-12-12 00:32:26 +0000

    Added validation to the address and description as well

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,5 @@
 class Place < ActiveRecord::Base
   belongs_to :user
-  validates :name, :presence => true
+  validates :name, :address, :presence => true
+  validates :description, :presence => true, :length => {minimum: 3}
 end
