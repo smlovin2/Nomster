95ea80398bcd739097729451fa6d9465b7470d53
Sean Lovinger <lovingersean01@gmail.com>
2015-12-12 20:36:47 +0000

    Set up the comment controller and create function

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Nomster::Application.routes.draw do
   root 'places#index'
   devise_for :users
 
-  resources :places
+  resources :places do
+    resources :comments, :only => :create
+  end
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
