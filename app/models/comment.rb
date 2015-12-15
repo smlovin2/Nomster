f4eb8b5f644478758bf06f590442b1015b29d370
Sean Lovinger <lovingersean01@gmail.com>
2015-12-15 04:10:31 +0000

    Added a callback method to send out the email when someone adds a comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
+  after_create :send_comment_email
 
   RATINGS = {
     'one star' => '1_star',
@@ -11,6 +12,10 @@ class Comment < ActiveRecord::Base
   }
 
   def humanized_rating
-    RATINGS.invert[self.rating]    
+    RATINGS.invert[self.rating]
+  end
+
+  def send_comment_email
+    NotificationMailer.comment_added(self).deliver
   end
 end
