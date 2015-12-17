064c33e96f72361f61f63cf4a542cc874a008376
Sean Lovinger <lovingersean01@gmail.com>
2015-12-17 00:06:05 +0000

    Added a couple of controler tests, one for user and one for comments

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create a comment" do
+    user = FactoryGirl.create(:user)
+    sign_in user
+    place = FactoryGirl.create(:place)
+
+    post :create, :place_id => place.id, :comment => {
+      :message => 'Yum!',
+      :rating => '5_stars',
+    }
+
+    assert_equal 1, place.comments.count
+    assert_redirected_to place_path(place)
+  end
 end
